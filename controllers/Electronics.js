104f0812fc552bf43317c74fbdbc4f2a2b6d7e84
Hema Pradeepthi <ghpradeepthi24@gmail.com>
2023-11-14 14:29:32 -0600

    latest changes

--- controllers/Electronics.js
+++ controllers/Electronics.js
@@ -44,3 +44,24 @@ exports.Electronics_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// Handle Costume create on POST.
+exports.Electronics_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new electronic();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.Gadget = req.body.Gadget;
+    document.Price = req.body.Price;
+    document.Storage = req.body.Storage;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
