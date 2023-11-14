db70d074b6404c2bcce3394528089d70dd9c03b8
Hema Pradeepthi <ghpradeepthi24@gmail.com>
2023-11-14 14:39:43 -0600

    latest changes

--- controllers/Electronics.js
+++ controllers/Electronics.js
@@ -65,3 +65,35 @@ exports.Electronics_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// for a specific Costume.
+exports.Electronics_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await electronic.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+// Handle Costume update form on PUT.
+exports.Electronics_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await electronic.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Gadget) toUpdate.Gadget = req.body.Gadget;
+        if (req.body.Price) toUpdate.Price = req.body.Price;
+        if (req.body.Storage) toUpdate.Storage = req.body.Storage;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
+};
